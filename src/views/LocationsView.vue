9dc7a0427a6bbcfcf749615132d7a4fb8ace1c7d
CavanBeardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-10-09 20:05:03 +0100

    added forecast, fixed toggles

--- src/views/LocationsView.vue
+++ src/views/LocationsView.vue
@@ -8,17 +8,21 @@
       <button @click="toggle('wind', 'm')" v-show="wind === 'k' && showingMore" class="toggle-button"> Show in Miles </button>
       <button @click="toggle('precip', 'mm')" v-show="precip === 'in' && showingMore" class="toggle-button"> Show in Millimeters </button>
       <button @click="toggle('precip', 'in')" v-show="precip === 'mm' && showingMore" class="toggle-button"> Show in Inches </button>
-      <button class="toggle-button" @click="toggle">Show More Information For All</button>
+      <button class="toggle-button" @click="toggle('on')" v-show="!showingAll">Show More Information For All</button>
+      <button class="toggle-button" @click="toggle('off')" v-show="showingAll">Hide More Information For All</button>
+      <button class="toggle-button" @click="toggle('four')" v-show="!fourHourly">Show Four Hourly Data</button>
+      <button class="toggle-button" @click="toggle('four')" v-show="fourHourly">Hide Four Hourly Data</button>
     </div>
     <div>
       <div class="location-list" v-for="location in locationsList" :key="location.name">
         <ul class="location" v-if="location.weatherData.location && location.weatherData.current">
           <h2>{{location.name}}</h2>
-          <button class="button">Get 3 Day Forecast</button>
+          <button class="button" v-show="showingType === 'current'" @click="toggle('type', 'forecast')">Show 3 Day Forecast Data</button>
+          <button class="button" v-show="showingType === 'forecast'" @click="toggle('type', 'current')">Show Current Data</button>
           <button class="button" @click="locationToggleMoreInfo(location)" v-show="!location.moreInfo">Show More Information</button>
           <button class="button" @click="locationToggleMoreInfo(location)" v-show="location.moreInfo">Hide More Information</button>
           <p v-if="requestError" style="color: red">{{requestError}}</p>
-          <table>
+          <table v-if="showingType === 'current'">
             <tr>
               <th>Time:</th>
               <th>Condition:</th>
@@ -51,6 +55,104 @@
               <td v-if="showMore || location.moreInfo" v-show="wind === 'k'">{{location.weatherData.current.vis_km}} KM</td>
             </tr>
           </table>
+          <div v-if="showingType === 'forecast'">
+            <div v-for="day in location.weatherData.forecast.forecastday" :key="day" class="forecast-day">
+              <h4>{{day.date}}</h4>
+              <table v-if="!fourHourly">
+                <tr>
+                  <th>Condition:</th>
+                  <th>Maximum Temperature:</th>
+                  <th>Chance Of Rain:</th>
+                  <th v-if="showMore || location.moreInfo">Chance of Snow:</th>
+                  <th v-if="showMore || location.moreInfo">Average Temperature:</th>
+                  <th v-if="showMore || location.moreInfo">Minimum Temperature:</th>
+                  <th v-if="showMore || location.moreInfo">Average Humidity:</th>
+                  <th v-if="showMore || location.moreInfo">Maximum Wind Speed:</th>
+                  <th v-if="showMore || location.moreInfo">Total Precipitation:</th>
+                  <th v-if="showMore || location.moreInfo">Average Visibility:</th>
+                  <th v-if="showMore || location.moreInfo">UV:</th>
+                </tr>
+                <tr>
+                  <td><img :src="day.day.condition.icon"></td>
+                  <td v-show="temperature === 'c'">{{day.day.maxtemp_c}} °C</td>
+                  <td v-show="temperature === 'f'">{{day.day.maxtemp_f}} °F</td>
+                  <td>{{day.day.daily_chance_of_rain}}%</td>
+                  <td v-if="showMore || location.moreInfo">{{day.day.daily_chance_of_snow}}%</td>
+                  <td v-if="showMore || location.moreInfo" v-show="temperature === 'c'">{{day.day.avgtemp_c}} °C</td>
+                  <td v-if="showMore || location.moreInfo" v-show="temperature === 'f'">{{day.day.avgtemp_f}} °F</td>
+                  <td v-if="showMore || location.moreInfo" v-show="temperature === 'c'">{{day.day.mintemp_c}} °C</td>
+                  <td v-if="showMore || location.moreInfo" v-show="temperature === 'f'">{{day.day.mintemp_f}} °F</td>
+                  <td v-if="showMore || location.moreInfo">{{day.day.avghumidity}}%</td>
+                  <td v-if="showMore || location.moreInfo" v-show="wind === 'k'">{{day.day.maxwind_kph}} KPH</td>
+                  <td v-if="showMore || location.moreInfo" v-show="wind === 'm'">{{day.day.maxwind_mph}} MPH</td>
+                  <td v-if="showMore || location.moreInfo" v-show="precip === 'mm'">{{day.day.totalprecip_mm}} mm</td>
+                  <td v-if="showMore || location.moreInfo" v-show="precip === 'in'">{{day.day.totalprecip_in}} inches</td>
+                  <td v-if="showMore || location.moreInfo" v-show="wind === 'k'">{{day.day.avgvis_km}} km</td>
+                  <td v-if="showMore || location.moreInfo" v-show="wind === 'm'">{{day.day.avgvis_miles}} miles</td>
+                  <td v-if="showMore || location.moreInfo">{{day.day.uv}}</td>
+                </tr>
+              </table>
+              <br v-if="astro">
+              <table v-if="astro">
+                <tr>
+                  <th>Sunrise:</th>
+                  <th>Sunset:</th>
+                  <th>Moonrise:</th>
+                  <th>Moonset:</th>
+                  <th>Moon Phase:</th>
+                  <th>Moon Illumination:</th>
+                </tr>
+                <tr>
+                  <td>{{day.astro.sunrise}}</td>
+                  <td>{{day.astro.sunset}}</td>
+                  <td>{{day.astro.moonrise}}</td>
+                  <td>{{day.astro.moonset}}</td>
+                  <td>{{day.astro.moon_phase}}</td>
+                  <td>{{day.astro.moon_illumination}}%</td>
+                </tr>
+              </table>
+              <br v-if="astro">
+              <div v-for="hour in day.hour" :key="hour">
+                <table v-if="fourHourly" v-show="multipleOf4(hour.time.split(' ')[1])">
+                  <tr>
+                    <th>Time:</th>
+                    <th>Condition:</th>
+                    <th>Temperature:</th>
+                    <th>Chance Of Rain:</th>
+                    <th v-if="location.moreInfo">Chance Of Snow:</th>
+                    <th v-if="location.moreInfo">Cloud Coverage:</th>
+                    <th v-if="location.moreInfo">Feels Like:</th>
+                    <th v-if="location.moreInfo">Humidity:</th>
+                    <th v-if="location.moreInfo">Precipitation:</th>
+                    <th v-if="location.moreInfo">UV:</th>
+                    <th v-if="location.moreInfo">Visibility:</th>
+                    <th v-if="location.moreInfo">Wind Speed:</th>
+                    <th v-if="location.moreInfo">Wind Direction:</th>
+                  </tr>
+                  <tr>
+                    <td>{{hour.time.split(' ')[1]}}</td>
+                    <td><img :src="hour.condition.icon"></td>
+                    <td>{{hour.temp_c}} °C</td>
+                    <td>{{hour.chance_of_rain}}%</td>
+                    <td v-if="location.moreInfo">{{hour.chance_of_snow}}%</td>
+                    <td v-if="location.moreInfo">{{hour.cloud}}%</td>
+                    <td v-if="location.moreInfo" v-show="temperature === 'c'">{{hour.feelslike_c}} °C</td>
+                    <td v-if="location.moreInfo" v-show="temperature === 'f'">{{hour.feelslike_f}} °F</td>
+                    <td v-if="location.moreInfo">{{hour.humidity}}%</td>
+                    <td v-if="location.moreInfo" v-show="precip === 'in'">{{hour.precip_in}} inches</td>
+                    <td v-if="location.moreInfo" v-show="precip === 'mm'">{{hour.precip_mm}} mm</td>
+                    <td v-if="location.moreInfo">{{hour.uv}}</td>
+                    <td v-if="location.moreInfo" v-show="wind === 'k'">{{hour.vis_km}} km</td>
+                    <td v-if="location.moreInfo" v-show="wind === 'm'">{{hour.vis_miles}} miles</td>
+                    <td v-if="location.moreInfo" v-show="wind === 'k'">{{hour.wind_kph}} KPH</td>
+                    <td v-if="location.moreInfo" v-show="wind === 'm'">{{hour.wind_mph}} MPH</td>
+                    <td v-if="location.moreInfo">{{hour.wind_dir}}</td>
+                  </tr>
+                </table>
+                <!-- <br v-if="fourHourly" v-show="multipleOf4(hour.time.split(' ')[1])"> -->
+              </div>
+            </div>
+          </div>
         </ul>
       </div>
     </div>
@@ -60,7 +162,7 @@
 <script lang="ts">
 import store from '@/store'
 import { useStore } from 'vuex'
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, onUpdated } from 'vue'
 import { getData } from '../functions/async'
 import { Location } from '../classes/locationClass'
 import type { Ref } from 'vue'
@@ -74,6 +176,11 @@ export default {
     //boolean
     const showMore: Ref<boolean> = ref(false);
     const showingMore: Ref<boolean> = ref(false);
+    const showingAll: Ref<boolean> = ref(false);
+    const fourHourly: Ref<boolean> = ref(false);
+
+    //strings
+    const showingType: Ref<string> = ref('current');
     
     //computed
     const locationsList = computed<[Location]>(() => store.getters.getLocations)
@@ -101,6 +208,24 @@ export default {
       }
     })
 
+    onUpdated(() => {
+      let areAllShowing: number = 0
+
+      for (let i = 0; i < locationsList.value.length; i++) {
+        if (locationsList.value[i].moreInfo === true) {
+          areAllShowing++
+        }
+      }
+
+      if (areAllShowing === locationsList.value.length) {
+        showingAll.value = true
+        areAllShowing = 0
+      } else {
+        showingAll.value = false
+        areAllShowing = 0
+      }
+    })
+
     function toggle(m: string, v: string) {
       if (m === 'temp') {
         store.commit('updateTemp', v )
@@ -108,9 +233,25 @@ export default {
         store.commit('updateWind', v)
       } else if (m === 'precip') {
         store.commit('updatePrecip', v)
-      } else {
-        showMore.value = !showMore.value
-        showingMore.value = !showingMore.value
+      } else if (m === 'type') {
+        showingType.value = v
+      } else if (m === 'on'){
+        for (var i = 0; i < locationsList.value.length; i++) {
+          if (locationsList.value[i].moreInfo === false) {
+            locationsList.value[i].toggleMoreInfo()
+            showingAll.value = true
+          }
+        }
+      } else if (m === 'off'){
+        for (var i = 0; i < locationsList.value.length; i++) {
+          if (locationsList.value[i].moreInfo === true) {
+            locationsList.value[i].toggleMoreInfo()
+            showingAll.value = false
+          }
+        }
+      } else if (m === 'four') {
+        fourHourly.value = !fourHourly.value
+        console.log(fourHourly.value)
       }
     }
 
@@ -118,6 +259,26 @@ export default {
       location.toggleMoreInfo()
       showingMore.value = !showingMore.value
     } 
+
+    function multipleOf4(hour: string){
+      if (hour === '00:00'){
+        return true
+      } else if (hour === '04:00'){
+        return true
+      } else if (hour === '08:00'){
+        return true
+      } else if (hour === '12:00'){
+        return true
+      } else if (hour === '16:00'){
+        return true
+      } else if (hour === '20:00'){
+        return true
+      } else if (hour === '23:00'){
+        return true
+      } else {
+        return false
+      }
+    }
 
     return {
       locationsList,
@@ -128,7 +289,11 @@ export default {
       toggle,
       showMore,
       locationToggleMoreInfo,
-      showingMore
+      showingMore,
+      showingType,
+      showingAll,
+      fourHourly,
+      multipleOf4
     }
   }
 }
